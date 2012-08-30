971e0fbbb31ed6bfc7ad9e4baf980830e71568c8
O. Ielfimov <oleksii.ielfimov@gmail.com>
2012-08-30 16:54:02 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Economy::Application.config.secret_token = '24faac9bccd7949b7d0a38fbc7d80973cf4a05eef3ffc0b05f3c55f2df1e5b9ef0f98374375d2d5d0e2678e943ed04fad20cede6f1cdfde31237e32092f2fa41'
